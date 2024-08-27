ea713180711a328f99d94dfa7885b6a4eeaba0d6
Ankur Verma <ankurverma7619@gmail.com>
2024-08-27 21:48:10 +0530

    changed names

--- src/constants/index.js
+++ src/constants/index.js
@@ -2,9 +2,9 @@ import project1 from "../assets/projects/project-1.jpg";
 import project2 from "../assets/projects/project-2.jpg";
 import project3 from "../assets/projects/project-3.jpg";
 // import project4 from "../assets/projects/project-4.jpg";
-import LeetCodeLogo from "../assets/LeetCode.jpg";
-import GFGLogo from "../assets/GFG.jpg";
-import CodeChefLogo from "../assets/CodeChef.jpg";
+import LeetCodeLogo from "../assets/leetcode.jpg";
+import GFGLogo from "../assets/gfg.jpg";
+import CodeChefLogo from "../assets/codechef.jpg";
 
 export const HERO_CONTENT = `I am a passionate full stack developer with a knack for crafting robust and scalable web applications.I have honed my skills in front-end technologies like React and Next.js, as well as back-end technologies like Node.js, MySQL, and MongoDB. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
 
